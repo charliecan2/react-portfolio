ce819a699801c5c7a49c5eff7b0aee8ab9a647ec
ccantu20 <carloscq920@gmail.com>
2021-04-14 12:38:09 -0500

    Updated portfolio project cards

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,39 +11,39 @@ function Portfolio(){
                 </div>
             </div>
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-sm-6">
                     <ProjectCard 
                     title={"Night Out"}
-                    imagesrc={"https://raw.githubusercontent.com/charliecan2/charliecan2.github.io/main/images/screenshots/NightOut.png"}
+                    imagesrc={`${process.env.PUBLIC_URL}/assets/images/NightOut.png`}
                     description={"For the undecided couple, group of friends, or family members that don't know what to do on their Night Out. This web-based application lets users search for Breweries and Events by city. Not satifsfied with your results? Simply hit the search button again, as each search is randomized!"}
                     githubLink={"https://github.com/jwhendershott/NightOut.git"}
                     deployedLink={"https://jwhendershott.github.io/NightOut"}
                     />
                 </div>
-                <div className="col-md-6">
+                <div className="col-sm-6">
                     <ProjectCard 
-                    title={"Movie Blog"}
-                    imagesrc={"https://raw.githubusercontent.com/charliecan2/charliecan2.github.io/main/images/screenshots/MovieBlog.png"}
-                    description={"The one stop website for Movie/TV Show enthusiasts. Movie Blog lets you search for where your favorite movies or shows are currently streaming. You can also write a mini-review by leaving a comment on the comments section of whatever movie you searched."}
-                    githubLink={"https://github.com/dcastelline/Movie-Blog.git"}
-                    deployedLink={"https://basic-movie-blog.herokuapp.com/"}
+                    title={"Resu-Make"}
+                    imagesrc={`${process.env.PUBLIC_URL}/assets/images/Resu-Make.png`}
+                    description={"Want an easy way to create a resume and easily access it on the web? That's where Resu-Make comes in! This application guides you in making a resume and allows for you to download it as a PDF after completion. You don't even have to be logged in to view your resume. Simply type in /resume/@youremail@example.com onto the url and you'll be directed to it!"}
+                    githubLink={"https://github.com/UT-final-project/group1-final-project"}
+                    deployedLink={"https://resumake-pro.herokuapp.com/"}
                     />
                 </div>
             </div>
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-sm-6">
                     <ProjectCard 
                     title={"Note Taker"}
-                    imagesrc={"https://raw.githubusercontent.com/charliecan2/charliecan2.github.io/main/images/screenshots/NoteTaker.png"}
+                    imagesrc={`${process.env.PUBLIC_URL}/assets/images/NoteTaker.png`}
                     description={"An application that lets you take down notes with a Title and Body. Clicking on the floppy disk on the upper-right saves your note. Notes can be viewed again by clicking on them on the left-hand side of the screen."}
                     githubLink={"https://github.com/charliecan2/NoteTaker.git"}
                     deployedLink={"https://note-taker-express-cc.herokuapp.com/"}
                     />
                 </div>
-                <div className="col-md-6">
+                <div className="col-sm-6">
                     <ProjectCard 
                     title={"Weather Board"}
-                    imagesrc={"https://raw.githubusercontent.com/charliecan2/charliecan2.github.io/main/images/screenshots/WeatherBoard.png"}
+                    imagesrc={`${process.env.PUBLIC_URL}/assets/images/WeatherBoard.png`}
                     description={"This Weather Board application lets you see the current Weather in any city you'd like. It even gives you a 5-day forecast to see what the upcoming week will be like! Don't see a city you're nearby? No worries. You can search for a new city in the search bar and your desired city will then show up in the city selection."}
                     githubLink={"https://github.com/charliecan2/WeatherDashboard.git"}
                     deployedLink={"https://charliecan2.github.io/WeatherDashboard/"}
@@ -51,19 +51,19 @@ function Portfolio(){
                 </div>
             </div>
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-sm-6">
                     <ProjectCard 
                     title={"Employee Directory"}
-                    imagesrc={"https://raw.githubusercontent.com/charliecan2/charliecan2.github.io/main/images/screenshots/EmployeeDirecotory.png"}
+                    imagesrc={`${process.env.PUBLIC_URL}/assets/images/EmployeeDirectory.png`}
                     description={"A react application that allows employers/managers to look up their employees. Users can sort their employees by their first/last name in alphabetical order and can even look up a specific employee by typing thier first/last name onto the search bar."}
                     githubLink={"https://github.com/charliecan2/employee-directory"}
                     deployedLink={"https://charliecan2.github.io/employee-directory/"}
                     />
                 </div>
-                <div className="col-md-6">
+                <div className="col-sm-6">
                     <ProjectCard 
                     title={"Workout Log"}
-                    imagesrc={"https://raw.githubusercontent.com/charliecan2/charliecan2.github.io/main/images/screenshots/WorkoutTracker.png"}
+                    imagesrc={`${process.env.PUBLIC_URL}/assets/images/WorkoutTracker.png`}
                     description={"This Workout Tracker allows users to log their exercises and review what their workout session looked like. Total weight lifted is displayed on a bar chart and the total workout duration is shown as well. The user's different workouts are also displayed and divided in a pie chart."}
                     githubLink={"https://github.com/charliecan2/WorkoutLog"}
                     deployedLink={"https://workout-log-app.herokuapp.com/"}
